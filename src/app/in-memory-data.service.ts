e1df7a4979789ebe29a27ebe0817d428de31ba0d
iamrajendra <rajendrarajaram@hotmail.com>
2020-09-05 13:46:30 +0530

    Added dynamic image in card

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,16 +8,16 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Randeep Hooda',nickName:"Randeep",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+      { id: 12, name: 'Arjun Rampal',nickName:"Arjun",url:"https://www.filmcompanion.in/wp-content/uploads/2017/09/arjun-rampal-interview-film-companion-feature-29.jpeg" },
+      { id: 13, name: 'Sylvester Stallone',nickName:"Rocky",url:"https://sm.ign.com/t/ign_it/blogroll/r/rambo-new-/rambo-new-blood-tv-series-in-development-at-fox_fgu5.2560.jpg" },
+      { id: 14, name: 'Danial Drag',nickName:"James Bond" ,url:"https://pmcvariety.files.wordpress.com/2019/12/b25_25594_r-e1575469208183.jpg?w=681&h=383&crop=1"},
+      { id: 15, name: 'Irfan Khan',nickName:"Ranvijay",url:"https://s01.sgp1.cdn.digitaloceanspaces.com/article/133654-lwoaaihvpg-1577542502.jpg" },
+      { id: 16, name: 'Imran Hasmi',nickName:"Arjun",url:"http://www.yusrablog.com/actors/imran-hashmi-25-latest-pictures-collection/attachment/emran-hashmi-indian-actor/" },
+      { id: 17, name: 'Priyanka Chopra',nickName:"merry kom",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+      { id: 18, name: 'Sharukh khan',nickName:"Raj" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
+      { id: 19, name: 'Amir Khan',nickName:"RJ" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
+      { id: 20, name: 'Amitabh Bachann',nickName:"Vijay" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"}
     ];
     return {heroes};
   }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+  { id: 11, name: 'Randeep Hooda',nickName:"Randeep",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+  { id: 12, name: 'Arjun Rampal',nickName:"Arjun",url:"https://images.mid-day.com/images/2019/sep/Arjun-Rampal-thrill_d.jpg" },
+  { id: 13, name: 'Sylvester Stallone',nickName:"Rocky",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+  { id: 14, name: 'Danial Drag',nickName:"James Bond" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
+  { id: 15, name: 'Irfan Khan',nickName:"Ranvijay",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+  { id: 16, name: 'Imran Hasmi',nickName:"Arjun",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+  { id: 17, name: 'Priyanka Chopra',nickName:"merry kom",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
+  { id: 18, name: 'Sharukh khan',nickName:"Raj" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
+  { id: 19, name: 'Amir Khan',nickName:"RJ" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
+  { id: 20, name: 'Amitabh Bachann',nickName:"Vijay" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"}];
