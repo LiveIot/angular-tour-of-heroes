68e95891e4cd35109529a2e29d808f2b33a77512
iamrajendra <rajendrarajaram@hotmail.com>
2020-09-05 23:12:23 +0530

    changes into material

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -14,10 +14,10 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 14, name: 'Danial Drag',nickName:"James Bond" ,url:"https://pmcvariety.files.wordpress.com/2019/12/b25_25594_r-e1575469208183.jpg?w=681&h=383&crop=1"},
       { id: 15, name: 'Irfan Khan',nickName:"Ranvijay",url:"https://s01.sgp1.cdn.digitaloceanspaces.com/article/133654-lwoaaihvpg-1577542502.jpg" },
       { id: 16, name: 'Imran Hasmi',nickName:"Arjun",url:"http://www.yusrablog.com/actors/imran-hashmi-25-latest-pictures-collection/attachment/emran-hashmi-indian-actor/" },
-      { id: 17, name: 'Priyanka Chopra',nickName:"merry kom",url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg" },
-      { id: 18, name: 'Sharukh khan',nickName:"Raj" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
-      { id: 19, name: 'Amir Khan',nickName:"RJ" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"},
-      { id: 20, name: 'Amitabh Bachann',nickName:"Vijay" ,url:"https://pbs.twimg.com/profile_images/1277973190220767232/Qhx1m6qs_400x400.jpg"}
+      { id: 17, name: 'Priyanka Chopra',nickName:"merry kom",url:"https://www.geo.tv/assets/uploads/updates/2020-09-03/305919_2343124_updates.jpg" },
+      { id: 18, name: 'Sharukh khan',nickName:"Kabir Khan" ,url:"https://images.outlookindia.com/public/uploads/articles/2020/2/6/Shahrukh_Khan_570_850.jpg"},
+      { id: 19, name: 'Amir Khan',nickName:"RJ" ,url:"https://gumlet.assettype.com/nationalherald%2F2020-05%2F3accc8c2-4c78-4ae6-a387-b0588272a973%2FAamir_Khan.jpg?rect=0%2C0%2C1280%2C720&auto=format%2Ccompress&format=webp&w=750&dpr=1.5"},
+      { id: 20, name: 'Amitabh Bachann',nickName:"Vijay" ,url:"https://s01.sgp1.digitaloceanspaces.com/large/853677-70527-jmxjmwlevh-1507480566.jpg"}
     ];
     return {heroes};
   }
